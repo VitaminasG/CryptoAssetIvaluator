0d86f80cbf9645f22e2ebb2d08f4c14e33f20338
Gediminas <gediminas.palsys@iterato.lt>
2023-11-07 00:07:23 +0200

    Adjust env file to be able connect to db and fake user fixture

---
+++ src/Entity/Asset.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AssetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: AssetRepository::class)]
+#[ApiResource]
+class Asset
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $currency = null;
+
+    #[ORM\Column]
+    #[Assert\GreaterThanOrEqual(
+        value: 0,
+        message: "Value cannot be negative"
+    )]
+    private ?float $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getCurrency(): ?string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(string $currency): static
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
